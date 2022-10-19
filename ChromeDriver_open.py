9173fa0ba48537720089e41521d98f54e4c713aa
AngusYeh <114390009+AngusYeh@users.noreply.github.com>
2022-10-19 22:27:13 +0800

    Update and rename tset.py to ChromeDriver_open.py

--- ChromeDriver_open.py
+++ ChromeDriver_open.py
@@ -2,6 +2,8 @@
 from selenium import webdriver
 import os
 
+#設定chromedriver.exe 路徑，driver的版本要隨時更新，以免不支援。
 os.environ["PATH"] = r"C:\SeleniumDrivers"
+
 driver = webdriver.Chrome()
 driver.get("https://www.google.com")
